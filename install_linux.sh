c8b9012e1a68fde08cc202508070cce2f893afcd
aiprime <zheng.fang@ai-prime.ai>
2025-01-08 16:18:41 +0800

    add auto script

---
+++ install_linux.sh
@@ -0,0 +1,7 @@
+sh tools/install.sh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+cp ./.zshrc ~/.zshrc
+source ~/.zshrc
